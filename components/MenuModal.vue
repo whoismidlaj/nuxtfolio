b2608e7ba80158710594813c05149a1d9c4658d0
whoismidlaj <its.me.midlaj@gmail.com>
2024-06-23 04:10:20 +0530

    0001

---
+++ components/MenuModal.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-screen h-screen flex justify-center items-center bg-gray-700/30 backdrop-blur-sm px-5">
+        <div class="w-full bg-gray-300/10 dark:bg-gray-700/50 backdrop-blur border border-gray-900/10 dark:border-gray-300/10 rounded-xl overflow-clip">
+            <div class="flex flex-col">
+                <NuxtLink :class="{'bg-gray-300/10 dark:bg-gray-500/30': $route.path === '/'}" class="px-5 py-4 border-b border-gray-900/10 dark:border-gray-300/10" to="/">Home</NuxtLink>
+                <NuxtLink :class="{'bg-gray-300/10 dark:bg-gray-500/30': $route.path === '/about'}" class="px-5 py-4 border-gray-900/10 dark:border-gray-300/10" to="/about">About</NuxtLink>
+            </div>
+        </div>
+    </div>
+</template>
--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,15 +1,25 @@
 <script setup>
+const showMenuModal = ref(false)
+function toggleMenu() {
+  showMenuModal.value = !showMenuModal.value
+}
 </script>
 
 <template>
     <nav class="fixed bottom-0 z-50 w-full py-5 border-t backdrop-blur border-gray-900/10 dark:border-gray-300/10 bg-gradient-to-tr from-gray-200/50 to-gray-300/50 dark:from-gray-800/50 dark:to-gray-900/50">
         <div class="container px-5 mx-auto flex justify-between">
-            <div class="menu flex space-x-5 text-slate-100">
+            <div class="menu hidden space-x-5 text-slate-100 md:flex">
                 <NuxtLink :class="{'btn-primary active': $route.path === '/'}" class="btn btn-primary" to="/">Home</NuxtLink>
                 <NuxtLink :class="{'btn-primary active': $route.path === '/about'}" class="btn btn-primary" to="/about">About</NuxtLink>
                 <!-- <NuxtLink :class="{'btn-primary active': $route.path === '/photodump'}" class="btn btn-primary" to="/photodump">Photos</NuxtLink> -->
             </div>
+            <div @click="toggleMenu" class="menuMobile flex justify-center items-center btn btn-primary p-2 md:hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-gray-900 dark:text-gray-200">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                </svg>
+            </div>
             <DarkModeToggle />
         </div>
     </nav>
+    <MenuModal v-show="showMenuModal" /> 
 </template>
