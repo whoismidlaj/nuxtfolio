37455d20e2523c5fee30f1dd9e14170234f3fdbb
whoismidlaj <its.me.midlaj@gmail.com>
2024-07-11 17:53:18 +0530

    Projects is live. beg fixes

--- pages/projects.vue
+++ pages/projects.vue
@@ -19,13 +19,13 @@
                         </div>
                     </div> -->
                     <div class="flex gap-2">
-                        <a href="#" target="_blank" class="icontext-btn">
+                        <a href="../midlaj-resume.pdf" target="_blank" class="icontext-btn">
                             <span class="hidden md:block">Resume</span>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
                             </svg>
                         </a>
-                        <a href="#" target="_blank" class="icontext-btn">
+                        <a href="mailto:midlaj@tuta.io" target="_blank" class="icontext-btn">
                             <span class="hidden md:block">Email</span>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
