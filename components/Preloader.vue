b475e0ec8c6c710b066dc8c63a5f9706c032745a
whoismidlaj <its.me.midlaj@gmail.com>
2024-07-11 17:29:29 +0530

    Projects is live. beg fixes

--- components/Preloader.vue
+++ components/Preloader.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="fixed top-0 left-0 w-screen h-screen bg-gray-300 text-gray-900 dark:bg-gray-900 dark:text-gray-300 bg-gradient-to-tr from-gray-200 to-gray-300 dark:from-gray-800 dark:to-gray-900">
-        <div class="w-full h-full flex justify-center items-center">
-            <div class="text-4xl">{{ progress }}%</div>
+        <div class="w-full h-full p-10 flex justify-end md:justify-center items-end md:items-center">
+            <div class="text-5xl md:text-6xl">{{ progress }}%</div>
         </div>
     </div>
   </template>
@@ -11,7 +11,7 @@
   
   const simulateLoading = async () => {
     const increment = 2; // Adjust increment for smoother progress
-    const interval = 20; // Adjust interval for desired speed (in milliseconds)
+    const interval = 10; // Adjust interval for desired speed (in milliseconds)
     const minDisplayTime = 500; // Minimum display time (in milliseconds)
   
     let intervalId = setInterval(() => {
@@ -31,26 +31,4 @@
     await simulateLoading();
   });
   </script>
-  
-  <style scoped>
-  .preloader {
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-color: rgba(0, 0, 0, 0.5); /* Adjust background color as needed */
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    z-index: 9999; /* Ensure preloader is above other content */
-  }
-  
-  .preloader-bar {
-    height: 5px; /* Adjust preloader bar height */
-    background-color: #f44336; /* Adjust progress bar color */
-    width: 0;
-    transition: width 0.2s ease-in-out; /* Add a smooth transition */
-  }
-  </style>
   
--- pages/projects.vue
+++ pages/projects.vue
@@ -5,6 +5,36 @@
         </div>
         <div class="w-full grid grid-cols-1 md:grid-cols-2 gap-4 justify-center">
             <WorkCard />
+            
+            <!-- Placeholder -->
+            <div class="flex flex-col justify-between border rounded-xl overflow-hidden bg-gray-300/30 dark:bg-gray-800/50 border-gray-900/10 dark:border-gray-300/10">
+                <div class="flex flex-col p-5 gap-2">
+                    <h3 class="text-xl">Ready to enhance your digital presence?</h3>
+                    <p>I specialize in creating responsive, user-friendly websites and web apps, available for hire.</p>
+                </div>
+                <div class="px-5 py-3 flex justify-end border-t bg-gray-300/30 dark:bg-gray-800 border-gray-900/10 dark:border-gray-300/10">
+                    <!-- <div class="flex items-center gap-4">
+                        <div class="technology-icon">
+                            <img class="w-7 h-7 bg-gray-300 rounded-full" src="../assets/img/profile-pic.png" alt="" srcset="">
+                        </div>
+                    </div> -->
+                    <div class="flex gap-2">
+                        <a href="#" target="_blank" class="icontext-btn">
+                            <span class="hidden md:block">Resume</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
+                            </svg>
+                        </a>
+                        <a href="#" target="_blank" class="icontext-btn">
+                            <span class="hidden md:block">Email</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <!-- Placeholder -->
         </div>
     </div>
 </template>
