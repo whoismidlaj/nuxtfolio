67bd740a6001769da0c7ecc93f954383ef454636
whoismidlaj <its.me.midlaj@gmail.com>
2024-07-12 11:18:49 +0530

    Projects is live. beg fixess

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -23,7 +23,7 @@ onUnmounted(() => {
 <template>
     <nav class="fixed bottom-0 z-50 w-full py-3 md:py-5 border-t backdrop-blur border-gray-900/10 dark:border-gray-300/10 bg-gradient-to-tr from-gray-200/10 to-gray-300/10 dark:from-gray-800/10 dark:to-gray-900/10">
         <div class="container px-5 mx-auto flex justify-between">
-            <div class="menu hidden space-x-5 text-slate-100 md:flex items-center">
+            <div class="menu hidden space-x-5 text-gray-800 dark:text-gray-100 md:flex items-center">
                 <NuxtLink :class="{'btn-primary active': $route.path === '/'}" class="btn btn-primary" to="/">Home</NuxtLink>
                 <NuxtLink :class="{'btn-primary active': $route.path === '/about'}" class="btn btn-primary" to="/about">About</NuxtLink>
                 <NuxtLink :class="{'btn-primary active': $route.path === '/projects'}" class="btn btn-primary" to="/projects">Projects</NuxtLink>
--- components/Preloader.vue
+++ components/Preloader.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="fixed top-0 left-0 w-screen h-screen bg-gray-300 text-gray-900 dark:bg-gray-900 dark:text-gray-300 bg-gradient-to-tr from-gray-200 to-gray-300 dark:from-gray-800 dark:to-gray-900">
-        <div class="w-full h-full p-10 flex justify-end md:justify-center items-end md:items-center">
-            <div class="text-5xl md:text-6xl">{{ progress }}%</div>
+        <div class="w-full h-full p-10 flex justify-center items-center">
+            <div class="text-4xl md:text-6xl">{{ progress }}%</div>
         </div>
     </div>
   </template>
